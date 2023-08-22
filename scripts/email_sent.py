cea601a9b0b21a188cfecb795c7262f8dd65c117
Usama shahid <us323619@gmail.com>
2023-08-22 18:40:01 +0500

    adding DNS record

--- scripts/email_sent.py
+++ scripts/email_sent.py
@@ -4,7 +4,7 @@ from email.mime.text import MIMEText
 
 # Email configuration
 sender_email = "us323619@gmail.com"
-receiver_email = "farmann@gmail.com"
+receiver_email = "us323619@gmail.com"
 subject = "Static Website HTML"
 
 # Read HTML content from index.html
